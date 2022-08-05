f88632c210cb3f4df63357b44c86555cc94bad62
dilusha07 <dilushasandaruwani07@gmail.com>
2022-08-05 21:41:34 +0530

    Give data to the server

--- script.js
+++ script.js
@@ -4,6 +4,15 @@ const Joi = require("joi"); // Import Joi
 const app = express(); //Create Express Application on the app variable
 app.use(express.json()); //Used the json file
 
+//Give data to the server
+const customers = [
+  { title: "Dilu", id: 1 },
+  { title: "Snada", id: 2 },
+  { title: "Chari", id: 3 },
+  { title: "Nilakshi", id: 4 },
+  { title: "Ravi", id: 5 },
+];
+
 //PORT environment variable
 const port = process.env.PORT || 8080;
 app.listen(port, () => console.log(`Listening on port ${port}...`));
